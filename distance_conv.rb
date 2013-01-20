13e70be84c451f80f4e8703bc1bc1add1e8a7c82
Todd Nichols <nivram80@gmail.com>
2013-01-19 23:07:40 -0600

    added comments

--- distance_conv.rb
+++ distance_conv.rb
@@ -1,16 +1,19 @@
 require 'sinatra'
 require './convert'
 
+# show the homepage
 get '/' do
   erb :index
 end
 
+# convert miles to kilometers page
 post '/to_km' do
   @m = MilesToKm.new(params[:miles])
   @now_km = @m.miles_to_km
   erb :to_km
 end
 
+# convert kilometers to miles page
 post '/to_miles' do
   @km = KmToMiles.new(params[:kilometers])
   @now_miles = @km.km_to_miles
